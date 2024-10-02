2cccad82a31719971429a0cef0c83c18d2152e62
Dmitry <dmitry@goverland.xyz>
2024-10-02 16:05:05 +0300

    Fix migration name

--- resources/V14_dao_active_proposals.sql
+++ resources/V14_dao_active_proposals.sql
@@ -1,13 +1,13 @@
 alter table daos
     add active_proposals_ids jsonb default '[]' not null;
 
-update storage.daos
+update daos
 set active_votes = cnt.active_votes, active_proposals_ids = cnt.list
 from (select
           dao_id,
           count(id) as active_votes,
           json_agg(id) list
-      from storage.proposals
+      from proposals
       where state = 'active' and spam is not true
       group by dao_id
       having count(id) > 0) cnt
